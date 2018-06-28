71bdf71b9273809eaa040e5feaae709973114218
Pedro Arthur <pedroarthur-mf@hotmail.com>
2018-06-28 20:26:34 -0300

    Sending file to AWS S3!

--- photos_webpage/views.py
+++ photos_webpage/views.py
@@ -1,4 +1,5 @@
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPSeeOther
 
 
 @view_config(route_name='home', renderer='templates/mytemplate.jinja2')
@@ -10,8 +11,18 @@ def my_view(request):
 def galery(request):
     return {'project': 'Photos'}
 
+@view_config(route_name='update',
+             request_method='POST',
+             renderer='templates/update.jinja2')
+def update_photo(request):
+    file = request.params.get('photo', '')
+    request.storage.save(request.POST['photo'], replace=True)
+    print HTTPSeeOther(request.route_url('update'))
+    return HTTPSeeOther(request.route_url('update'))
+
+
 @view_config(route_name='update', renderer='templates/update.jinja2')
-def update(request):
+def update_page(request):
     return {'project': 'Photos'}
 
 @view_config(route_name='select', renderer='templates/select.jinja2')
